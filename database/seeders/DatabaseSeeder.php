2e12a8fc3c2294d9e043162e694f8904465b5774
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-01-23 13:07:27 +0100

    Seeder de User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(CompaniaSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(EspecialidadSeeder::class);
         $this->call(EspecialistaSeeder::class);
     }

2e1ff563a46bf101f334857928dc032ef768ea63
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-01-23 13:02:55 +0100

    Llamada para que pueda hacer el seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(CompaniaSeeder::class);
+        $this->call(EspecialidadSeeder::class);
+        $this->call(EspecialistaSeeder::class);
     }
 }
